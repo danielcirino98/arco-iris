4ffe09e76d467607e5c47a50d290d8fa5ff8a049
danielcirino98 <109473604+danielcirino98@users.noreply.github.com>
2023-11-25 19:23:07 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,118 @@
+
+
+function Mostrar(){
+
+ let m = document.getElementById("div1");
+ m.style.backgroundColor = "red";
+ console.log("Vermelho selecionado");
+
+}
+
+function Mostrar2(){
+
+   let m2 = document.getElementById("div2");
+   m2.style.backgroundColor = "orange";
+   console.log("Laranja selecionado");
+  
+  }
+  
+  function Mostrar3(){
+
+   let m3 = document.getElementById("div3");
+   m3.style.backgroundColor = "yellow";
+   console.log("Amarelo selecionado");
+  
+  }
+
+  function Mostrar4(){
+
+   let m4 = document.getElementById("div4");
+   m4.style.backgroundColor = "green";
+   console.log("Verde selecionado");
+  
+  }
+
+  function Mostrar5(){
+
+   let m5 = document.getElementById("div5");
+   m5.style.backgroundColor = "blue";
+   console.log("Azul selecionado");
+  
+  }
+  
+  function Mostrar6(){
+
+   let m6 = document.getElementById("div6");
+   m6.style.backgroundColor = " #1d1a44 ";
+   console.log( "Anil selecionado");
+  
+  }
+
+  function Mostrar7(){
+   let m7 = document.getElementById("div7");
+   m7.style.backgroundColor = "purple";
+   console.log("Roxo selecionado");
+  
+  }
+
+
+  /* Não mostrar */
+
+function nmostrar(){
+
+   let m = document.getElementById("div1");
+   m.style.backgroundColor = "white";
+  
+  }
+  
+  function nmostrar2(){
+  
+     let m2 = document.getElementById("div2");
+     m2.style.backgroundColor = "white";
+    
+    }
+    
+    function nmostrar3(){
+  
+     let m3 = document.getElementById("div3");
+     m3.style.backgroundColor = "white";
+    
+    }
+  
+    function nmostrar4(){
+  
+     let m4 = document.getElementById("div4");
+     m4.style.backgroundColor = "white";
+    
+    }
+  
+    function nmostrar5(){
+  
+     let m5 = document.getElementById("div5");
+     m5.style.backgroundColor = "white";
+    
+    }
+    
+    function nmostrar6(){
+  
+     let m6 = document.getElementById("div6");
+     m6.style.backgroundColor = " white ";
+    
+    }
+  
+    function nmostrar7(){
+  
+     let m7 = document.getElementById("div7");
+     m7.style.backgroundColor = "white";
+    
+    }
+
+    function clicou(){
+
+
+
+      m7.style.backgroundColor = "purple";
+    }
+    
+  
+  
